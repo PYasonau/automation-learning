559b591a13c4211fb16ad01432bbdcf2e78fbecb
Pavel Yasonau <megomozg@gmail.com>
2019-05-08 13:40:16 +0300

    sample framework

---
+++ AutomationFramework/AutomationFramework/Tests/SampleTest.cs
@@ -0,0 +1,16 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AutomationFramework.Tests
+{
+    public class SampleTest
+    {
+        [Test]
+        public void Sample()
+        {
+            Assert.That(true, Is.True);
+        }
+    }
+}
