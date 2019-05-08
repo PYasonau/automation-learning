ef973d2f9d0ebf56ab4ba16eb69d553e8543c845
Pavel Yasonau <megomozg@gmail.com>
2019-05-08 14:28:26 +0300

    locators for header of nbc.com

---
+++ AutomationFramework/AutomationFramework/Pages/NBCHeader.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AutomationFramework.Pages
+{
+    public class NBCHeader
+    {
+        public static string lnkShows = "a[href='/shows']";
+        public static string lnkEpisodes = "a[href='/video']";
+        public static string lnkSchedule = "a[href='/schedule']";
+        public static string lnkNewsSports = "//*[contains(text(), 'Sports')]/.."; //XPath locator
+        public static string lnkShop = "a[href$='shop']"; //ends with shop
+        public static string lnkApp = "a[href='/apps']";
+        public static string btnSearch = "[class$='search'] button";
+    }
+}
