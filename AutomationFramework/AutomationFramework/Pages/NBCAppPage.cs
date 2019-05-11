3fa6a51ebd952c64b1bbbcc7d2d5ec6419dd8646
Pavel Yasonau <megomozg@gmail.com>
2019-05-11 14:55:15 +0300

    PO and Flow tests samples

---
+++ AutomationFramework/AutomationFramework/Pages/NBCAppPage.cs
@@ -0,0 +1,25 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AutomationFramework.Pages
+{
+    public class NBCAppPage
+    {
+        private IWebDriver _driver;
+
+        private IWebElement lblAndroidAppHeader => _driver.FindElement(By.XPath("//div[text()='Android App']"));
+        public NBCAppPage(IWebDriver driver)
+        {
+            _driver = driver;
+        }
+
+        public bool IsAndroidAppBlockDisplayed()
+        {
+            new WebDriverWait(_driver, TimeSpan.FromSeconds(30)).Until(d => lblAndroidAppHeader.Displayed);
+            return true;
+        }
+    }
+}
---
+++ AutomationFramework/AutomationFramework/Tests/BaseTest.cs
@@ -0,0 +1,31 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Remote;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AutomationFramework.Tests
+{
+    [Parallelizable(ParallelScope.Fixtures)]
+    public class BaseTest
+    {
+        protected IWebDriver Driver1;
+        protected IWebDriver Driver2;
+        [SetUp]
+        public void CreateDrivers()
+        {
+            Driver1 = new RemoteWebDriver(new ChromeOptions { });
+            Driver1.Manage().Window.Maximize();
+        }
+
+        [TearDown]
+        public void CloseDrivers()
+        {
+            Driver1?.Quit();
+            Driver2?.Quit();
+        }
+
+    }
+}
